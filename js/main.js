c17646a3cfe8f5caf81e6a5234b3750486bf9348
Will McGIll <willmcgill@Makiyas-iPhone.localdomain>
2019-09-24 10:41:42 -0400

    created api pull

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
-function changeBodyBg(color){
-    document.body.style.background = color;
+//set const to URL API
+
+const URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+const apiKey = ",us&APPID=b767ab5374552a0f308785e30d6322ab";
+
+//set zipcode
+
+var zipCode = 40505;
+
+//Pull data from open weather
+
+function getMyData() {fetch(URL + zipCode + apiKey) //pull from dynamic URL
+  .then(function(x) {                        //pull response and convert to json
+    console.log(x.json);
+   return x.json();                          //return response
+  })
+  .then(function(jsonData) {                        //console log returned data
+    console.log(JSON.stringify(jsonData));
+    var city = jsonData.name;
+    console.log(city);
+  })
+  .then(function(weather){
+    //var city = weather.name;
+    console.log("city");
+  })
+  ;
+
 }
 
-changeBodyBg();
+getMyData();
