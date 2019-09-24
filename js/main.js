56c3517f5ad656c552f6e7af0c9e802bbedf9f98
Will McGIll <willmcgill@Makiyas-iPhone.localdomain>
2019-09-24 13:45:51 -0400

    created API and pulling into HTML

--- js/main.js
+++ js/main.js
@@ -1,30 +1,48 @@
-//set const to URL API
 
-const URL = "http://api.openweathermap.org/data/2.5/weather?q=";
-const apiKey = ",us&APPID=b767ab5374552a0f308785e30d6322ab";
+document.getElementById("button-addon1").addEventListener("click", getMyData);
 
-//set zipcode
+var cityName = " "
 
-var zipCode = 40505;
-
-//Pull data from open weather
-
-function getMyData() {fetch(URL + zipCode + apiKey) //pull from dynamic URL
-  .then(function(x) {                        //pull response and convert to json
-    console.log(x.json);
-   return x.json();                          //return response
-  })
-  .then(function(jsonData) {                        //console log returned data
-    console.log(JSON.stringify(jsonData));
-    var city = jsonData.name;
-    console.log(city);
-  })
-  .then(function(weather){
-    //var city = weather.name;
-    console.log("city");
-  })
-  ;
-
+function returnData(){
+   document.getElementById("cityName").innerHTML = cityName;
+   document.getElementById("cityTemp").innerHTML = cityTempF;
+   console.log("return Data function");
+   console.log(cityName);
 }
 
-getMyData();
+function getMyData() {
+  console.log("button pressed, here is the data");
+  //set const to URL API
+
+  const URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+  const apiKey = ",us&APPID=b767ab5374552a0f308785e30d6322ab";
+
+  //set zipcode
+
+  var zipCode = document.getElementById("zipForm").value;
+  console.log(zipCode);
+
+  fetch(URL + zipCode + apiKey) //pull from dynamic URL
+    .then(function (x) {                        //pull response and convert to json
+
+      console.log(zipCode);
+      return x.json();                          //return response
+    })
+    .then(function (jsonData) {                        //console log returned data
+      console.log(JSON.stringify(jsonData));
+      cityName = jsonData.name;
+      cityTempK = (jsonData.main.temp).toFixed(1) + " °K";
+      cityTempC = (jsonData.main.temp - 273.15).toFixed(1) + "°C";
+      cityTempF = ((jsonData.main.temp - 273.15) * 9/5 + 32).toFixed(1) + " °F";
+      console.log(cityTemp);
+
+      returnData();
+    })
+    
+    //document.getElementById("cityName").innerHTML = cityName;
+    }
+    ;
+
+
+
+
